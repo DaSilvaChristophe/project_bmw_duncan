a9e5ad785ec50064a232934ecfe5b01893932a6e
Christophe Da Silva <crisinvest88@gmail.com>
2022-10-31 01:56:45 +0100

    index html/css validé W3C - index JS ok - responsive ok

--- js/index.js
+++ js/index.js
@@ -1,24 +1,22 @@
 const inputLoa = document.querySelector('.loa')
 const svgLoa = document.getElementById('path10531-1')
 
-function mouseOver(event) {
-    console.log(event);
-    inputLoa.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br/> sed do eiusmod tempor incididunt";
+const mouseOver = () => {
+  inputLoa.classList.toggle("active");
+    inputLoa.innerHTML = "Lorem ipsum dolor sit amet, consectetur <br> elit, sed do eiusmod tempor incididunt";
     inputLoa.style.fontSize = "0.85em"
+    inputLoa.style.paddingRight = "30px"
     svgLoa.style.fill = "#fff";
     svgLoa.style.opacity= "1";
-    inputLoa.style.transition = "0.7s ease-in-out 0s"
   }
 
-function mouseOut(event) {
-    console.log(event);
+const mouseOut = () => {
     inputLoa.innerHTML = "LOA";
     inputLoa.style.fontSize = "1em"
     svgLoa.style.fill = "#0066b1";
     svgLoa.style.opacity= "0.5";
-    inputLoa.style.transition = " 0.6s ease-in-out 0s"
   }
 
 inputLoa.addEventListener('click', () => {
-  window.location.href = "summary-loa.html"
+  window.location.href = "2-summary-loa.html"
 })
