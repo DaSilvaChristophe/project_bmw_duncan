48f2b0e920d6375824428a22d642ae633bd35c97
Christophe Da Silva <crisinvest88@gmail.com>
2022-11-07 00:26:48 +0100

    desktop and mobile version ok nav ? validation W3C HTML and CSS ok

--- js/index.js
+++ js/index.js
@@ -1,23 +1,24 @@
-const inputLoa = document.querySelector('.loa')
-const svgLoa = document.getElementById('path10531-1')
+const inputLoa = document.querySelector(".loa");
+const svgLoa = document.getElementById("path10531-1");
 
 const mouseOver = () => {
   inputLoa.classList.toggle("active");
-    inputLoa.innerHTML = "Lorem ipsum dolor sit amet, consectetur <br> elit, sed do eiusmod tempor incididunt";
-    inputLoa.style.fontSize = "0.85em";
-    inputLoa.style.paddingRight = "30px";
-    svgLoa.style.fill = "#fff";
-    svgLoa.style.opacity= "1";
-  }
+  inputLoa.innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur <br> elit, sed do eiusmod tempor incididunt";
+  inputLoa.style.fontSize = "0.85em";
+  inputLoa.style.paddingRight = "30px";
+  svgLoa.style.fill = "#fff";
+  svgLoa.style.opacity = "1";
+};
 
 const mouseOut = () => {
-    inputLoa.innerHTML = "LOA";
-    inputLoa.style.fontSize = "1em";
-    inputLoa.style.paddingRight = "0px";
-    svgLoa.style.fill = "#0066b1";
-    svgLoa.style.opacity= "0.5";
-  }
+  inputLoa.innerHTML = "LOA";
+  inputLoa.style.fontSize = "1em";
+  inputLoa.style.paddingRight = "0px";
+  svgLoa.style.fill = "#0066b1";
+  svgLoa.style.opacity = "0.5";
+};
 
-inputLoa.addEventListener('click', () => {
-  window.location.href = "2-summary-loa.html"
-})
+inputLoa.addEventListener("click", () => {
+  window.location.href = "2-summary-loa.html";
+});
